16dcca79210a40d5cd4e982028c5047d61f5a369
Muhammad Tio Saputra <2511500058@mahasiswa.atmaluhur.ac.id>
2026-04-24 16:01:58 +0700

    mengedit dan memperbarui mapel.php

--- page/mapel.php
+++ page/mapel.php
@@ -1,64 +1,63 @@
-<div class="content-header">
-    <div class="container-fluid">
-        <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1 class="m-0">Data Mapel</h1>
-            </div>
-        </div>
-    </div>
-</div>
-
 <?php
-if (isset($_GET['action'])) {
-    if ($_GET['action'] == "hapus") {
-        $kd = $_GET['kd'];
-        $query = mysqli_query($koneksi, "DELETE FROM mapel WHERE kd_mapel = '$kd'");
-        if ($query) {
-            echo '<div class="alert alert-success alert-dismissible">Data Berhasil Dihapus!</div>';
-            // Perbaikan: Redirect ke halaman mapel, bukan edit_mapel
-            echo '<meta http-equiv="refresh" content="1; url=index.php?page=mapel">';
-        }
-    }
+if (isset($_GET['action']) == "hapus") {
+    $kd_mapel = $_GET['kd'];
+    mysqli_query($koneksi, "DELETE FROM mapel WHERE kd_mapel='$kd_mapel'");
+    echo '<div class="alert alert-success">Data berhasil dihapus!</div>';
 }
 ?>
 
-<div class="content">
-    <div class="container-fluid">
-        <div class="card">
+<div class="data">
+    <div class="col-12">
+        <div class="card card-primary card-outline">
+            <div class="card-header">
+                <h3 class="card-title"><i class="fas fa-chalkboard-teacher"></i> Data Mata Pelajaran</h3>
+                <div class="card-tools">
+                    <a href="index.php?page=tambah_mapel" class="btn btn-sm btn-primary">
+                        <i class="fas fa-plus"></i> Tambah Mata Pelajaran
+                    </a>
+                </div>
+            </div>
             <div class="card-body">
-                <a href="index.php?page=tambah_mapel" class="btn btn-primary btn-sm mb-3">Tambah Data</a>
-                <table class="table table-striped table-bordered">
-                    <thead> <tr>
-                            <th>No</th>
-                            <th>Kd Mapel</th>
-                            <th>Nama Mapel</th>
-                            <th>KKM</th>
-                            <th>Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        $no = 0;
-                        $query = mysqli_query($koneksi, "SELECT * FROM mapel");
-                        while ($result = mysqli_fetch_array($query)) {
-                            $no++;
-                        ?>
-                        <tr>
-                            <td><?= $no; ?></td> <td><?= $result['kd_mapel']; ?></td>
-                            <td><?= $result['nm_mapel']; ?></td>
-                            <td><?= $result['kkm']; ?></td>
-                            <td>
-                                <a href="index.php?page=mapel&action=hapus&kd=<?= $result['kd_mapel'] ?>" onclick="return confirm('Yakin ingin menghapus?')">
-                                    <span class="badge badge-danger">Hapus</span>
-                                </a>
-                                <a href="index.php?page=edit_mapel&kd=<?= $result['kd_mapel'] ?>">
-                                    <span class="badge badge-warning">Edit</span>
-                                </a>
-                            </td>
-                        </tr>
-                        <?php } ?>
-                    </tbody>
-                </table>
+                <div class="table-responsive">
+                    <table class="table table-bordered table-striped table-datatable">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>KD Mapel</th>
+                                <th>Nama Mapel</th>
+                                <th>KKM</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $no = 1;
+                            $query = mysqli_query($koneksi, "SELECT * FROM mapel ORDER BY nm_mapel");
+                            while($data = mysqli_fetch_array($query)) {
+                            ?>
+                            <tr>
+                                <td><?php echo $no++; ?></td>
+                                <td><strong><?php echo $data['kd_mapel']; ?></strong></td>
+                                <td><?php echo $data['nm_mapel']; ?></td>
+                                <td><?php echo $data['kkm']; ?></td>
+                                
+                                <td>
+                                    <a href="index.php?page=edit_mapel&kd=<?php echo $data['kd_mapel']; ?>" 
+                                       class="btn btn-warning btn-sm" title="Edit">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+                                    <a href="index.php?page=mapel&action=hapus&kd=<?php echo $data['kd_mapel']; ?>" 
+                                       class="btn btn-danger btn-sm" 
+                                       onclick="return confirm('Yakin hapus?')"
+                                       title="Hapus">
+                                        <i class="fas fa-trash"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
